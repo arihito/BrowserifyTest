b5d9f605d93a9780437bbbe33ebb2d2d5039e27a
arihito <arihito.m@gmail.com>
2017-02-10 17:29:06 +0900

    module_split

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 // BrowserifyはCommonJSモジュール形式をサポート
 const load = require("lodash");
+// 外部モジュールの読み込み
+const list = require("./list");
 
+console.log(load.map(list, (i) => i + 1));
 console.log(load.map([1, 2], (i) => i + 1));
 
